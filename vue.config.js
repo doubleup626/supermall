20354e9c2527f8f7171459514a5bf2440ccc4b6c
double_up <784291885@qq.com>
2022-10-27 17:35:25 +0800

    项目前期准备工作完成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // '@':'src'   //默认有这个别名了
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'router': '@/router',
+        'store': '@/store',
+        'views': '@/views'   //但我觉得只起src的别名@就够用了
+      }
+    }
+  }
+}
